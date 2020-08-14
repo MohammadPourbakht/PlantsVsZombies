662d4b94c8f0ae7291d1277034fefcc5a6f68cbc
pc <mariavasajedeh@gmail.com>
2020-08-14 10:48:44 +0430

    Add constructor Controller Add Scroreboard Add ctimer add holder add scene add score

--- Controller.cpp
+++ Controller.cpp
@@ -2,5 +2,27 @@
 
 Controller::Controller(QObject *parent) : QObject(parent)
 {
+  //create scene
+    scene = new QGraphicsScene();
+    scene->setSceneRect(0,0,1200,700);
+
+    //create holder
+    holder = new QGraphicsRectItem();
+    holder->setRect(0,0,1200,700);
+
+    //create and start ctimer
+    ctimer = new QTimer();
+    ctimer->start(40);    // 50 | 40  ???
+
+    //add scoreBoard
+    scoreBoard = new QGraphicsPixmapItem();
+    scoreBoard->setPixmap(QPixmap(":/images/score.png"));
+    scene->addItem(scoreBoard);
+    scoreBoard->setPos(10,10);
+
+    //add score
+    controllerScore = new Score();
+    scene->addItem(controllerScore);
+    controllerScore->setPos(35,112);
 
 }

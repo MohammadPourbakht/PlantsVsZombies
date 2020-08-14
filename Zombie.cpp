440c782515319fb2f1c6f1a60d54ae4d3feaeee3
pc <mariavasajedeh@gmail.com>
2020-08-14 10:30:02 +0430

    Add Constructor Zombie in Zombie.cpp

--- Zombie.cpp
+++ Zombie.cpp
@@ -1,6 +1,14 @@
 #include "Zombie.h"
 
-Zombie::Zombie(QObject *parent) : QObject(parent)
+Zombie::Zombie(const int& pixPer40MiliSec , QTimer *timer
+               , QGraphicsItem *parent)
+    : QObject () , QGraphicsPixmapItem (parent) ,
+      pixPer40MiliSec{pixPer40MiliSec}
 {
+  //set picture
+   setPixmap(QPixmap(":/images/"));
+
+   //connect timer to moveToLeft
+   connect(timer , SIGNAL(timeout()) , this , SLOT(moveToLeft()));
 
 }

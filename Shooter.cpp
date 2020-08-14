8b0af29f8093464cf5ddbac96449a407cc63ca99
MohammadPourbakht <mohammad.poorbakhat.80@gmail.com>
2020-08-14 18:52:42 +0430

    add constructor to shooter class

--- Shooter.cpp
+++ Shooter.cpp
@@ -1,6 +1,43 @@
 #include "Shooter.h"
+#include<QGraphicsScene>
+#include"Bullet.h"
 
-Shooter::Shooter(QObject *parent) : QObject(parent)
+Shooter::Shooter(QMediaPlayer *shooterPlayer,QTimer * shooterTimer, QGraphicsItem *parent)
+    : QObject() , QGraphicsPixmapItem(parent) , Plant() , shooterTimer{shooterTimer}
 {
+    //set picture
+    setPixmap(QPixmap(":/images/shooter.png"));                 //image shooter
 
+    //create shooter player
+    shooterPlayer = new QMediaPlayer();
+    shooterPlayer->setMedia(QUrl("qrc:/music/Shooter.mp3"));        //sound shooter
+
+    //connect shoot slot
+    connect(shooterTimer , SIGNAL(timeout()) , this , SLOT(shoot()));
+
+}
+
+void Shooter::shoot()
+{
+    //shoot every 400 millisecond
+    if( timeIntervals % 10 ==0 ){
+
+        //create bullet
+        auto bullet = new Bullet(shooterTimer , 24);
+
+        scene()->addItem(bullet);
+        bullet->setPos(x()+ 90,y());
+
+        //plsy shooterPlayer
+        if(shooterPlayer->state() == QMediaPlayer::PlayingState){
+            shooterPlayer->setPosition(0);
+        }
+        else if(shooterPlayer->state() == QMediaPlayer::StoppedState){
+            shooterPlayer->play();
+        }
+
+    }
+
+    //invrement timeIntervals
+    ++timeIntervals;
 }
--- Shooter.h
+++ Shooter.h
@@ -2,14 +2,27 @@
 #define SHOOTER_H
 
 #include <QObject>
+#include <QGraphicsPixmapItem>
+#include<QMediaPlayer>
+#include "Plant.h"
 
-class Shooter : public QObject
+
+class Shooter : public QObject , public QGraphicsPixmapItem , public Plant
 {
     Q_OBJECT
+
+private:
+    QMediaPlayer * shooterPlayer;
+    int timeIntervals;
+    QTimer * shooterTimer;
+
 public:
-    explicit Shooter(QObject *parent = nullptr);
+    explicit Shooter(QMediaPlayer * shooterPlayer , QTimer * shooterTimer , QGraphicsItem *parent = nullptr);
 
 signals:
+
+public slots:
+    void shoot();
 
 };
 

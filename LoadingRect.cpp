cde2a6390eeac5c5313eb19220d07b13d766a434
MohammadPourbakht <mohammad.poorbakhat.80@gmail.com>
2020-08-15 13:53:06 +0430

    add constructor to LoadingRect class

--- LoadingRect.cpp
+++ LoadingRect.cpp
@@ -1,6 +1,14 @@
+#include<QGraphicsScene>
 #include "LoadingRect.h"
 
 LoadingRect::LoadingRect(QTimer *bulletTimer, const int &velocity)
+        : QObject() , velocity{velocity}
 {
+    //set picture
+    setPixmap(QPixmap(":/images/loadingRect.png"));
+
+    //connect to moveToRight
+    connect(bulletTimer , SIGNAL(timeout()) , this , SLOT(moveToRight()));
+
 
 }

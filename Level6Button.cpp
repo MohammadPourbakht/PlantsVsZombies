797a203ad45d28396dbb934e7566a89eada33102
BaharBehzadipour <bahar.b.2001.bb@gmail.com>
2020-08-17 02:25:48 +0430

    add constructor to Level6Button class

--- Level6Button.cpp
+++ Level6Button.cpp
@@ -1,6 +1,20 @@
 #include "Level6Button.h"
 
-Level6Button::Level6Button()
+Level6Button::Level6Button(QGraphicsScene *Scene)  : level6Scene{Scene}
 {
+    clickl6 = false;
+
+    //set picture
+    setPixmap(QPixmap(":/images/level6.png"));
+
+    //add to scene
+    Scene->addItem(this);
+
+    //setPos
+    setPos(540,477);
+
+    //create level5 button music
+       level6ButtonMusic = new QMediaPlayer();
+       level6ButtonMusic->setMedia(QUrl("qrc:/music/shooter.mp3"));
 
 }
--- Level6Button.h
+++ Level6Button.h
@@ -1,11 +1,23 @@
 #ifndef LEVEL6BUTTON_H
 #define LEVEL6BUTTON_H
 
+#include <QGraphicsScene>
+#include <QGraphicsSceneMouseEvent>
+#include <QMediaPlayer>
+#include <QGraphicsPixmapItem>
 
-class Level6Button
+class Level6Button  : public QGraphicsPixmapItem
 {
+    friend class Menu;
+private:
+    QGraphicsScene * level6Scene;
+    QMediaPlayer * level6ButtonMusic;
+    bool clickl6;
+
 public:
-    Level6Button();
+    Level6Button(QGraphicsScene *Scene);
+    void mousePressEvent(QGraphicsSceneMouseEvent *event);
+    ~Level6Button();
 };
 
 #endif // LEVEL6BUTTON_H

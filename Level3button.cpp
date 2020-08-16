e18517fd6afafee83efccf746a065f8075ce2e83
MohammadPourbakht <mohammad.poorbakhat.80@gmail.com>
2020-08-17 01:25:41 +0430

    add constructor to Level3Button class

--- Level3button.cpp
+++ Level3button.cpp
@@ -1,6 +1,21 @@
 #include "Level3button.h"
 
-Level3Button::Level3Button()
+
+Level3Button::Level3Button(QGraphicsScene *Scene)  : level3Scene{Scene}
 {
+    clickl3 = false;
+
+    //set picture
+    setPixmap(QPixmap(":/images/level3.png"));
+
+    //add to scene
+    Scene->addItem(this);
+
+    //setPos
+    setPos(500,450);
+
+    //create level2 button music
+       level3ButtonMusic = new QMediaPlayer();
+       level3ButtonMusic->setMedia(QUrl("qrc:/music/shooter.mp3"));
 
 }
--- Level3button.h
+++ Level3button.h
@@ -1,11 +1,24 @@
 #ifndef LEVEL3BUTTON_H
 #define LEVEL3BUTTON_H
 
+#include <QGraphicsScene>
+#include <QGraphicsSceneMouseEvent>
+#include <QMediaPlayer>
+#include <QGraphicsPixmapItem>
 
-class Level3Button
+
+class Level3Button  : public QGraphicsPixmapItem
 {
+    friend class Menu;
+private:
+    QGraphicsScene * level3Scene;
+    QMediaPlayer * level3ButtonMusic;
+    bool clickl3;
+
 public:
-    Level3Button();
+    Level3Button(QGraphicsScene *Scene);
+    void mousePressEvent(QGraphicsSceneMouseEvent *event);
+    ~Level3Button();
 };
 
 #endif // LEVEL3BUTTON_H

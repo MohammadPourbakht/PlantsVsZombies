f504857c920265e0a9784cd7c8eff823a8763be7
MohammadPourbakht <mohammad.poorbakhat.80@gmail.com>
2020-08-17 01:10:00 +0430

    add constructor to Level2Button class

--- Level2Button.cpp
+++ Level2Button.cpp
@@ -1,6 +1,20 @@
 #include "Level2Button.h"
 
-Level2Button::Level2Button()
+Level2Button::Level2Button(QGraphicsScene *Scene) : level2Scene{Scene}
 {
+    clickl2 = false;
+
+    //set picture
+    setPixmap(QPixmap(":/images/level2.png"));
+
+    //add to scene
+    Scene->addItem(this);
+
+    //setPos
+    setPos(450,480);
+
+    //create level2 button music
+       level2ButtonMusic = new QMediaPlayer();
+       level2ButtonMusic->setMedia(QUrl("qrc:/music/shooter.mp3"));
 
 }
--- Level2Button.h
+++ Level2Button.h
@@ -1,11 +1,25 @@
 #ifndef LEVEL2BUTTON_H
 #define LEVEL2BUTTON_H
 
+#include <QGraphicsPixmapItem>
+#include <QGraphicsScene>
+#include <QGraphicsSceneMouseEvent>
+#include <QMediaPlayer>
 
-class Level2Button
+
+class Level2Button :public QGraphicsPixmapItem
 {
+    friend class Menu;
+private:
+    QGraphicsScene * level2Scene;
+    QMediaPlayer * level2ButtonMusic;
+    bool clickl2;
+
 public:
-    Level2Button();
+    Level2Button(QGraphicsScene *Scene);
+    void mousePressEvent(QGraphicsSceneMouseEvent *event);
+    ~Level2Button();
+
 };
 
 #endif // LEVEL2BUTTON_H

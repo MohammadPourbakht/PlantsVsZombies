f51b7c1389245f575bb1b44419a5ffbc284dd498
pc <mariavasajedeh@gmail.com>
2020-08-18 00:18:26 +0430

    Add Constructor SunFlower.cpp

--- SunFlower.cpp
+++ SunFlower.cpp
@@ -1,6 +1,16 @@
 #include "SunFlower.h"
+#include "Sun.h"
 
-SunFlower::SunFlower(QObject *parent) : QObject(parent)
+
+SunFlower::SunFlower(Score *sunScore ,  QGraphicsScene *sunScene ,
+                    QTimer *sunTimer , QGraphicsItem *parent )
+ : QObject() , Plant() , sunTimer(sunTimer) , timeIntervals{0} , sunScore(sunScore)
+   , sunScene(sunScene)
 {
+    //set picture
+    setPixmap(QPixmap(":/images/Sunflower.png"));
+
+    connect(sunTimer,SIGNAL(timeout()),this,SLOT(makeSun()));
 
 }
+

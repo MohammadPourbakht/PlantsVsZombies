15528968d451f7424275a1a083facb65cfc6e960
MohammadPourbakht <mohammad.poorbakhat.80@gmail.com>
2020-08-17 22:39:39 +0430

    add destructor to  NutIcon class

--- NutIcon.cpp
+++ NutIcon.cpp
@@ -1,6 +1,24 @@
 #include "NutIcon.h"
+#include "Icons.h"
 
-NutIcon::NutIcon()
+NutIcon::NutIcon(QGraphicsScene *nutIconScene) : nutIconScene{nutIconScene}
 {
+    isSelected=false;
+    isSelectable=false;
 
+    //set picture
+    setPixmap(QPixmap(":/images/dark nut icon.png"));
+
+    //add to scene
+    nutIconScene->addItem(this);
+
+    //setPos
+    setPos(315 , 11);
+
+}
+
+
+NutIcon::~NutIcon()
+{
+    delete nutIconScene;
 }
--- NutIcon.h
+++ NutIcon.h
@@ -1,11 +1,22 @@
 #ifndef NUTICON_H
 #define NUTICON_H
 
+#include "Icons.h"
 
-class NutIcon
+class NutIcon : public QGraphicsPixmapItem , public Icons
 {
+    friend class Controller;
+private:
+    QGraphicsScene * nutIconScene;
+    bool isSelected;
+    bool isSelectable;
+
 public:
-    NutIcon();
+    NutIcon(QGraphicsScene *shooterIconScene);
+    ~NutIcon();
+    void mousePressEvent(QGraphicsSceneMouseEvent *event);
+    void setShooterIcon();
+
 };
 
 #endif // NUTICON_H

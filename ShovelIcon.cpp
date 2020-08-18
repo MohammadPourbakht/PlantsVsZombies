bef72d3459dafb09a662383f33b87d291c84a59e
MohammadPourbakht <mohammad.poorbakhat.80@gmail.com>
2020-08-18 14:09:21 +0430

    add mousePressEvent to ShovelIcon class

--- ShovelIcon.cpp
+++ ShovelIcon.cpp
@@ -13,11 +13,28 @@ ShovelIcon::ShovelIcon(QGraphicsScene *shovelIconScene) : shovelIconScene{shovel
     shovelIconScene->addItem(this);
 
     //setPos
-    setPos(730 , 11);
+    setPos(730 , 0);
 
 }
 
 ShovelIcon::~ShovelIcon()
 {
- delete shovelIconScene;
+    delete shovelIconScene;
 }
+
+void ShovelIcon::mousePressEvent(QGraphicsSceneMouseEvent *event)
+{
+    if(isSelectable==true){
+        isSelected=true;
+        //set picture
+        setPixmap(QPixmap(":/images/light shovel icon.png"));
+        //add to scene
+       // shooterIconScene->addItem(this);
+        //setPos
+        setPos(730 , 0);
+    }
+
+    //add sound if isSelectable == false
+
+}
+

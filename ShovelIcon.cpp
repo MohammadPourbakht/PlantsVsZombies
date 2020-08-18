7a9b9c8daa1139f6682dd23f2a9caba75cf1e7a8
MohammadPourbakht <mohammad.poorbakhat.80@gmail.com>
2020-08-18 14:07:17 +0430

    addd constructor to ShovelIcon class

--- ShovelIcon.cpp
+++ ShovelIcon.cpp
@@ -1,7 +1,18 @@
 #include "ShovelIcon.h"
 
 
-ShovelIcon::ShovelIcon()
+ShovelIcon::ShovelIcon(QGraphicsScene *shovelIconScene) : shovelIconScene{shovelIconScene}
 {
+    isSelected=false;
+    isSelectable=false;
+
+    //set picture
+    setPixmap(QPixmap(":/images/shovel icon.png"));
+
+    //add to scene
+    shovelIconScene->addItem(this);
+
+    //setPos
+    setPos(730 , 11);
 
 }
--- ShovelIcon.h
+++ ShovelIcon.h
@@ -1,11 +1,26 @@
 #ifndef SHOVELICON_H
 #define SHOVELICON_H
 
+#include <QGraphicsScene>
+#include <QGraphicsSceneMouseEvent>
+#include <QGraphicsPixmapItem>
 
-class ShovelIcon
+class ShovelIcon : public QGraphicsPixmapItem
 {
+    friend class Controller;
+private:
+    QGraphicsScene * shovelIconScene;
+    bool isSelected;
+    bool isSelectable;
 public:
-    ShovelIcon();
+    ShovelIcon(QGraphicsScene *shovelIconScene);
+    ~ShovelIcon();
+
+    void mousePressEvent(QGraphicsSceneMouseEvent *event);
+    void setShovelIcon();
 };
 
 #endif // SHOVELICON_H
+
+
+
